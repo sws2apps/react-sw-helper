b316b449ad37ea785f0afa9f6ecb1b74dfaeddd6
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2022-07-06 15:11:07 +0300

    fix(component): vite build issue with process.env.NODE_ENV

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+	define: {
+		'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+	},
 	build: {
 		lib: {
 			entry: path.resolve(__dirname, 'src/lib/index.jsx'),
