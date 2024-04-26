7a2c246a253ff36fd0e025f13c037690999e8345
rhahao <26148770+rhahao@users.noreply.github.com>
2024-04-26 23:45:31 +0300

    feat(library): update project to typescript

---
+++ src/lib/index.types.ts
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react';
+
+export type ServiceWorkerWrapperType = {
+	children: ((args: { update?: () => void }) => ReactNode) | ReactNode;
+	onError?: (error: Error) => void;
+	onInstalled?: () => void;
+	onUpdated?: () => void;
+	onWaiting?: () => void;
+	publicServiceWorkerDest: string;
+	publicUrl?: string;
+};
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
 	build: {
 		lib: {
-			entry: path.resolve(__dirname, 'src/lib/index.jsx'),
+			entry: path.resolve(__dirname, 'src/lib/index.tsx'),
 			name: 'ServiceWorkerWrapper',
 			fileName: (format) => `react-sw-helper.${format}.js`,
 		},
