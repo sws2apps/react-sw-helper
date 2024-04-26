5fd263d1fe6b6a7202449c7df75940d43579181a
rhahao <26148770+rhahao@users.noreply.github.com>
2024-04-26 23:45:31 +0300

    feat(library): update project to typescript

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,15 +1,10 @@
 import { useCallback, useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
+import { ServiceWorkerWrapperType } from './index.types';
 
 const isLocalhost = Boolean(
-	typeof window !== 'undefined' &&
-		(window.location.hostname === 'localhost' ||
-			// [::1] is the IPv6 localhost address.
-			window.location.hostname === '[::1]' ||
-			// 127.0.0.1/8 is considered localhost for IPv4.
-			window.location.hostname.match(
-				/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-			))
+	window?.location.hostname === 'localhost' ||
+		window?.location.hostname === '[::1]' ||
+		window?.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
 );
 
 const ServiceWorkerWrapper = ({
@@ -19,10 +14,11 @@ const ServiceWorkerWrapper = ({
 	onUpdated,
 	onWaiting,
 	publicServiceWorkerDest,
-	publicUrl,
-}) => {
-	const [registration, setRegistration] = useState(null);
-	const [serviceWorkerUrl] = useState(`${publicUrl}${publicServiceWorkerDest}`);
+	publicUrl = '',
+}: ServiceWorkerWrapperType) => {
+	const [registration, setRegistration] = useState<ServiceWorkerRegistration | null>(null);
+
+	const serviceWorkerUrl = `${publicUrl}${publicServiceWorkerDest}`;
 
 	const registerValidServiceWorker = useCallback(async () => {
 		try {
@@ -67,7 +63,7 @@ const ServiceWorkerWrapper = ({
 			});
 		} catch (err) {
 			console.error('Error during service worker registration:', err);
-			onError && onError(err);
+			onError && onError(err as Error);
 		}
 	}, [serviceWorkerUrl, onError, onWaiting, onInstalled, onUpdated]);
 
@@ -75,10 +71,7 @@ const ServiceWorkerWrapper = ({
 		try {
 			const response = await fetch(serviceWorkerUrl);
 			// Ensure service worker exists, and that we really are getting a JS file.
-			if (
-				response.status === 404 ||
-				response.headers.get('content-type').indexOf('javascript') === -1
-			) {
+			if (response.status === 404 || response.headers.get('content-type')!.indexOf('javascript') === -1) {
 				// No service worker found. Probably a different app. Reload the page.
 				navigator.serviceWorker.ready.then(async (registration) => {
 					await registration.unregister();
@@ -89,22 +82,22 @@ const ServiceWorkerWrapper = ({
 				registerValidServiceWorker();
 			}
 		} catch (err) {
-			console.log(
-				'No internet connection found. App is running in offline mode.'
-			);
+			console.log('No internet connection found. App is running in offline mode.');
 		}
 	}, [registerValidServiceWorker, serviceWorkerUrl]);
 
 	const register = useCallback(async () => {
 		if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
 			// The URL constructor is available in all browsers that support SW.
-			const pUrl = new URL(publicUrl, window.location);
+			const pUrl = new URL(publicUrl);
+
 			if (pUrl.origin !== window.location.origin) {
 				// Our service worker won't work if PUBLIC_URL is on a different origin
 				// from what our page is served on. This might happen if a CDN is used to
-				// serve assets; see https://github.com/facebookincubator/create-react-app/issues/2374
+				// serve assets
 				return;
 			}
+
 			if (isLocalhost) {
 				// This is running on localhost. Lets check if a service worker still exists or not.
 				checkValidServiceWorker();
@@ -112,8 +105,7 @@ const ServiceWorkerWrapper = ({
 				// service worker/PWA documentation.
 				navigator.serviceWorker.ready.then(() => {
 					console.log(
-						'This web app is being served cache-first by a service ' +
-							'worker. To learn more, visit https://goo.gl/SC7cgQ'
+						'This web app is being served cache-first by a service ' + 'worker. To learn more, visit https://goo.gl/SC7cgQ'
 					);
 				});
 			} else {
@@ -124,8 +116,10 @@ const ServiceWorkerWrapper = ({
 	}, [checkValidServiceWorker, publicUrl, registerValidServiceWorker]);
 
 	const update = useCallback(() => {
-		registration && registration.update();
-		registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+		if (registration) {
+			registration.update();
+			registration.waiting?.postMessage({ type: 'SKIP_WAITING' });
+		}
 	}, [registration]);
 
 	useEffect(() => {
@@ -134,27 +128,7 @@ const ServiceWorkerWrapper = ({
 		}
 	}, [register]);
 
-	return typeof children === 'function'
-		? children({ update: update })
-		: children;
-};
-
-ServiceWorkerWrapper.propTypes = {
-	children: PropTypes.oneOfType([PropTypes.func, PropTypes.node]).isRequired,
-	onError: PropTypes.func,
-	onInstalled: PropTypes.func,
-	onUpdated: PropTypes.func,
-	onWaiting: PropTypes.func,
-	publicServiceWorkerDest: PropTypes.string.isRequired,
-	publicUrl: PropTypes.string,
-};
-
-ServiceWorkerWrapper.defaultProps = {
-	onError: null,
-	onInstalled: null,
-	onUpdated: null,
-	onWaiting: null,
-	publicUrl: '',
+	return typeof children === 'function' ? children({ update: update }) : children;
 };
 
 export default ServiceWorkerWrapper;
