342aac7c64f3810f39648a9e356f9c360e737b58
rhahao <26148770+rhahao@users.noreply.github.com>
2025-02-27 11:35:42 +0300

    fix(module): check for waiting state on first load

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -2,86 +2,125 @@ import { useCallback, useEffect, useState } from 'react';
 import { ServiceWorkerWrapperType } from './index.types';
 
 const isLocalhost = Boolean(
-	window?.location.hostname === 'localhost' ||
-		window?.location.hostname === '[::1]' ||
-		window?.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
+  window?.location.hostname === 'localhost' ||
+    window?.location.hostname === '[::1]' ||
+    window?.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
 );
 
 const ServiceWorkerWrapper = ({
-	children,
-	onError,
-	onInstalled,
-	onUpdated,
-	onWaiting,
-	publicServiceWorkerDest,
-	publicUrl = '',
+  children,
+  onError,
+  onInstalled,
+  onUpdated,
+  onWaiting,
+  publicServiceWorkerDest,
+  publicUrl = '',
 }: ServiceWorkerWrapperType) => {
-	const [registration, setRegistration] = useState<ServiceWorkerRegistration | null>(null);
+  const [registration, setRegistration] =
+    useState<ServiceWorkerRegistration | null>(null);
 
-	const registerServiceWorker = useCallback(async () => {
-		try {
-			const registration =
-				(await navigator.serviceWorker.getRegistration()) ||
-				(await navigator.serviceWorker.register(`${publicUrl}${publicServiceWorkerDest}`));
-			setRegistration(registration);
+  const registerServiceWorker = useCallback(async () => {
+    try {
+      const registration =
+        (await navigator.serviceWorker.getRegistration()) ||
+        (await navigator.serviceWorker.register(
+          `${publicUrl}${publicServiceWorkerDest}`
+        ));
 
-			registration.addEventListener('updatefound', () => {
-				const worker = registration.installing || registration.waiting;
-				worker?.addEventListener('statechange', () => {
-					if (worker.state === 'installed') {
-						navigator.serviceWorker.controller ? onUpdated?.() : onInstalled?.();
-					}
-				});
-			});
-		} catch (err) {
-			console.error('Error during service worker registration:', err);
-			onError?.(err as Error);
-		}
-	}, [publicUrl, publicServiceWorkerDest, onError, onWaiting, onInstalled, onUpdated]);
+      setRegistration(registration);
 
-	const checkServiceWorker = useCallback(async () => {
-		try {
-			const response = await fetch(`${publicUrl}${publicServiceWorkerDest}`);
-			if (response.status === 404 || !response.headers.get('content-type')?.includes('javascript')) {
-				const registration = await navigator.serviceWorker.ready;
-				await registration.unregister();
-				window.location.reload();
-			} else {
-				registerServiceWorker();
-			}
-		} catch (err) {
-			console.log('No internet connection found. App is running in offline mode.');
-		}
-	}, [publicUrl, publicServiceWorkerDest, registerServiceWorker]);
+      if (registration?.waiting) {
+        console.log('Service Worker is in waiting state.');
+        onUpdated?.();
+      }
 
-	const register = useCallback(() => {
-		if ('serviceWorker' in navigator) {
-			if (new URL(publicUrl, window.location.href).origin !== window.location.origin) {
-				console.warn('Service worker cannot be registered because PUBLIC_URL is on a different origin from page.');
-				return;
-			}
+      registration.addEventListener('updatefound', () => {
+        const worker = registration.installing || registration.waiting;
+        worker?.addEventListener('statechange', () => {
+          if (worker.state === 'installed') {
+            if (navigator.serviceWorker.controller) {
+              onUpdated?.();
+            } else {
+              onInstalled?.();
+            }
+          }
+        });
+      });
+    } catch (err) {
+      console.error('Error during service worker registration:', err);
+      onError?.(err as Error);
+    }
+  }, [
+    publicUrl,
+    publicServiceWorkerDest,
+    onError,
+    onWaiting,
+    onInstalled,
+    onUpdated,
+  ]);
 
-			isLocalhost ? checkServiceWorker() : registerServiceWorker();
-		}
-	}, [publicUrl, checkServiceWorker, registerServiceWorker]);
+  const checkServiceWorker = useCallback(async () => {
+    try {
+      const response = await fetch(`${publicUrl}${publicServiceWorkerDest}`);
+      if (
+        response.status === 404 ||
+        !response.headers.get('content-type')?.includes('javascript')
+      ) {
+        const registration = await navigator.serviceWorker.ready;
+        await registration.unregister();
+        window.location.reload();
+      } else {
+        registerServiceWorker();
+      }
+    } catch (err) {
+      console.log(
+        'No internet connection found. App is running in offline mode.',
+        err
+      );
+    }
+  }, [publicUrl, publicServiceWorkerDest, registerServiceWorker]);
 
-	const update = useCallback(() => {
-		if (registration) {
-			registration.update();
+  const register = useCallback(() => {
+    if ('serviceWorker' in navigator) {
+      if (
+        new URL(publicUrl, window.location.href).origin !==
+        window.location.origin
+      ) {
+        console.warn(
+          'Service worker cannot be registered because PUBLIC_URL is on a different origin from page.'
+        );
+        return;
+      }
 
-			if (registration.waiting) {
-				registration.waiting.postMessage({ type: 'SKIP_WAITING' });
-			}
-		}
-	}, [registration]);
+      if (isLocalhost) {
+        checkServiceWorker();
+      } else {
+        registerServiceWorker();
+      }
+    }
+  }, [publicUrl, checkServiceWorker, registerServiceWorker]);
 
-	useEffect(() => {
-		if (process.env['NODE_ENV'] === 'production') {
-			register();
-		}
-	}, [register]);
+  const update = useCallback(() => {
+    if (registration) {
+      registration.update();
 
-	return typeof children === 'function' ? children({ update: update }) : children;
+      if (registration.waiting) {
+        registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+      }
+    }
+  }, [registration]);
+
+  useEffect(() => {
+    if (process.env['NODE_ENV'] === 'production') {
+      register();
+    }
+  }, [register]);
+
+  return typeof children === 'function'
+    ? children({ update: update })
+    : children;
 };
 
 export default ServiceWorkerWrapper;
