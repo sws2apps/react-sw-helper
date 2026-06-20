c429a4d07a0dfadbbd557198c90b3d05aafe91e5
rhahao <26148770+rhahao@users.noreply.github.com>
2026-06-21 00:02:52 +0300

    build(deps): bump typescript from 5.9.3 to 6.0.3

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -3,10 +3,10 @@ import { ServiceWorkerWrapperType } from './index.types';
 
 const isLocalhost = Boolean(
   window?.location.hostname === 'localhost' ||
-    window?.location.hostname === '[::1]' ||
-    window?.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
+  window?.location.hostname === '[::1]' ||
+  window?.location.hostname.match(
+    /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+  )
 );
 
 const ServiceWorkerWrapper = ({
